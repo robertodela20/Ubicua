5f59860c2303b5add563c69248007b3af6f7daa2
paulo-gomez <72405414+paulo-gomez@users.noreply.github.com>
2023-04-18 15:51:15 +0200

    Interfaz con 2 botóns

---
+++ iCampus/Assets/Scripts/edificio.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class edificio : MonoBehaviour
+{
+    public Text Textfield;
+
+    public void SetText(){
+        //Textfield.text=text;
+        Textfield.text="Facultade de Matemáticas\nEdificio pechado\nHorario: 8:00-21:00";
+    }
+}
+
--- iCampus/Assets/Scripts/farola.cs
+++ iCampus/Assets/Scripts/farola.cs
@@ -2,13 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Linq;
 
 public class farola : MonoBehaviour
 {
     public Text Textfield;
 
-    public void SetText(string text){
-        Textfield.text=text;//"Farola apagada";
+    public void SetText(int id){
+        //Textfield.text=text;
+        Textfield.text="Farola apagada";
+        /*string path = @"C:\Users\34628\Documents\Ubicua\iCampus\Assets\Scripts\texto_botons.txt";
+        FileStream arquivo= File.Open(path,  FileMode.Open, FileAccess.Read);
+        IEnumerable<string> linhas= File.ReadLines(path);
+        List<string> lista_linhas= linhas.ToList();
+        Textfield.text= lista_linhas[id];
+        */
+
     }
 }
 
