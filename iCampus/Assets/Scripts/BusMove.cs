06e15a0f436c94601be75f578aec00a74ae13816
Pablo Trasbach Sánchez <72377040+pablotr21@users.noreply.github.com>
2023-05-22 10:04:12 +0200

    Línea de bus entera
    Implementación completa de la línea de bus.

--- iCampus/Assets/Scripts/BusMove.cs
+++ iCampus/Assets/Scripts/BusMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,23 @@ using UnityEngine.AI;
 
 public class BusMove : MonoBehaviour
 {
-    [SerializeField] 
-    Transform _destination;
+    //Public variables
+    /*[SerializeField] 
+    Transform _FinalDestination;*/
+    public int BusId;
 
+    [SerializeField]
+    Transform[] points;
+
+    //[SerializeField]
+    //float totalWaitTime = 3f;
+    
+    //private variables
     NavMeshAgent _navMeshAgent;
+    int destPoint;
+    bool _travelling;
+    //bool _waiting;
+    //float _waitTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -21,17 +35,56 @@ public class BusMove : MonoBehaviour
             Debug.LogError("El componente navMeshAgent no está enlazado con " + gameObject.name);
         }
         else{
-            SetDestination();
+            if (points.Length > 0){
+                _navMeshAgent.autoBraking = false;
+                _navMeshAgent.updateRotation = false;
+		        _navMeshAgent.updateUpAxis = false;
+                GotoNextPoint();
+            }
         }
     }
 
-    // Set destination target
+
+    // Select the next point in the rute
+    private void GotoNextPoint() {
+        // Set the agent to go to the currently selected destination.
+        Vector3 targetDestination = points[destPoint].transform.position;
+        _navMeshAgent.SetDestination(targetDestination);
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+        _travelling = true;
+    }
+
+    /* Set the final destination target
     private void SetDestination()
     {
-        if(_destination != null)
+        if(_FinalDestination != null)
         {
-            Vector3 targetVector = _destination.transform.position;
+            Vector3 targetVector = _FinalDestination.transform.position;
             _navMeshAgent.SetDestination(targetVector);
+        }
+    }*/
+
+    /*private void setPointsBus()
+    {
+        if(this.BusId == 0){
+
+        }
+    }*/
+
+    // Choose the next destination point when the agent gets
+    // close to the current one.
+    void Update () {
+        if (_travelling && (_navMeshAgent.remainingDistance <= 7.0f) && !_navMeshAgent.pathPending){
+            _travelling = false;
+            Debug.Log(_navMeshAgent.remainingDistance);
+            //Wait in busStop
+            /*if(_navMeshAgent.){
+
+            }*/
+
+            GotoNextPoint();
         }
     }
 }
